0b2fa55a622f25cb4d1e6629ec253c62a7dbd853
CesarHernandez90 <cesarendirecto@gmail.com>
2019-06-15 12:00:12 -0500

    [Correción] Se estableció la longitud máxima de un String

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Blade::component('components/formActions', 'formActions');
         Blade::component('components/navbarTitle', 'navbarTitle');
     }
